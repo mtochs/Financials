91b7cc59d7e84625804a98a09c4d9773c12b719a
mtochs <mtochs@gmail.com>
2017-11-02 12:50:53 -0600

    updated plotting list in fin...py

--- fin_funcs/plot_spread.py
+++ fin_funcs/plot_spread.py
@@ -10,14 +10,14 @@ plt.style.use('ggplot')
 def plot_spread(tickers=['^IRX','^TYX'], start_date='1995-01-01',
 		end_date=datetime.datetime.today().strftime('%Y-%m-%d')):
 	df = pd.DataFrame(data=None, columns=['A', 'B', 'diff'])
-	df.B = pdr.get_data_yahoo(symbols=tickers[1], start=start_date, end=end_date)['Close']
-	df.A = pdr.get_data_yahoo(symbols=tickers[0], start=start_date, end=end_date)['Close']
-	df.diff = df.B - df.A
+	df.B = pd.to_numeric(pdr.get_data_yahoo(symbols=tickers[1], start=start_date, end=end_date)['Close'], downcast='float')
+	df.A = pd.to_numeric(pdr.get_data_yahoo(symbols=tickers[0], start=start_date, end=end_date)['Close'], downcast='float')
+	df['diff'] = df['B'].subtract(df['A'])
 	# Last data point	
-	last = df.diff.iloc[-1]
+	last = df.iloc[-1]['diff']
 
 	fig, ax = plt.subplots()
-	df.diff.plot(color='#5687d4')
+	df['diff'].plot(color='#5687d4')
 	# Horizontal line at last data point
 	plt.axhline(y=last, color='#d62728')
 	fig.set_size_inches(16, 9)
@@ -30,5 +30,5 @@ if __name__ == "__main__":
 	#	10 year '^TNX',
 	#	30 year '^TYX'
 	
-	df, plt = plot_spread()
+	df, plt = plot_spread(tickers=['^FVX','^TYX'])
 	plt.show()
--- finance.py
+++ finance.py
@@ -8,31 +8,69 @@ plt.style.use('ggplot')
 import  aws_funcs.plt_to_s3 as aws
 from fin_funcs.candlestick import candlstkr, candlstkr_ratio
 from fin_funcs.correlationify import correlationify
+from fin_funcs.margin_debt import update_margin_debt_s3 
 from fin_funcs.plot_spread import plot_spread
 
+candls = [
+		['^IXIC', 'candls_NASDAQ'], 
+		['^GSPC', 'candls_SP500'],
+		['^DJI', 'candls_DJI'],
+		['^TYX', 'candls_30yr'],
+		['^RUT', 'candls_Russel2000']
+	]
+
+ratios = [
+		['IVW','IVE', 'candlr_Growth_to_Value'], # S&P Growth to Value
+		['RSX', 'SPY', 'candlr_Russia_to_SP500'], # Russia to S&P500
+		['DBC','SPY', 'candlr_Commodities_to_SP500'], # Commodities to S&P
+		['TLT', 'SPY', 'candlr_Treasuries_to_SP500']
+	]
+
+spreads = [
+		['^FVX','^TYX', 'tspread_05-30.png'],
+		['^TNX','^TYX', 'tspread_10-30.png'],
+		['^IRX','^TNX', 'tspread_00-10.png']
+	]
+
+def make_candls(d):
+	for i in d:
+		df, plt = candlstkr(tick=i[0])
+		aws.upload_plt(plt, i[1] + '.png')
+
+def make_candls_ratios(d):
+	for i in d:
+		df, plt = candlstkr_ratio(tickers=i[0:2])
+		aws.upload_plt(plt, i[2] + '.png')
+
+def make_plot_spreads(d):
+	for i in d:
+		df, plt = plot_spread(tickers=i[0:2], start_date='1990-01-01')
+		aws.upload_plt(plt, i[2] + '.png')
 
 if __name__ == "__main__":
+	make_candls(candls)
+	make_candls_ratios(ratios)
+	make_plot_spreads(spreads)
+
 	#  Inputs
 	#   tick, start_date, end_date, freq
-	df_candl, plt = candlstkr(tick='^IXIC')
-	aws.upload_plt(plt, 'NASDAQ_candl.png')
+	#df, plt = candlstkr(tick='XLP', freq='W')
 	#plt.show()
-	#plt.clf()
 
 	#  Inputs
 	#   tickers=['A','B'], start_date, end_date, freq
-	#df_candl_ratio, plt = candlstkr_ratio(tickers=['DBC','SPY'])
-	#plt.show()
-	#plt.clf()
-
+	#df_candl_ratio, plt = candlstkr_ratio(tickers=['SPY', 'TLT'])
+	#plt.show(), plt.clf()
+	
 	#  Inputs
 	#   tickers=['A','B'], start_date, end_date
 	#   freq, rolling_corr, threshold
 	#df_corr, plt = correlationify()
-	#plt.show()
-	#plt.clf()
+	#plt.show(), plt.clf()
 
 	#  Inputs
 	#   tickers=['A','B'], start_date, end_date
 	#df_spread, plt = plot_spread()
-	#plt.show()
+	#plt.show(), plt.clf()
+
+	#df = update_margin_debt_s3()
